3ae46a14dd714a182bb7bf313e20c7bbfcc8ba85
Katelli <aska082024@gmail.com>
2024-08-26 14:42:35 +0200

    Completed test 4

--- main.js
+++ main.js
@@ -103,8 +103,8 @@ Example 2: ["One", "Two", "Three", "Four", "Five", "Six"] should return
 ["Two", "Three", "Four", "Five"]
 ******************************************************************************/
 
-export function arrayTrimmer() {
-  //your code here
+export function arrayTrimmer(array) {
+  return array.slice(1, -1)
 }
 
 /******************************************************************************
